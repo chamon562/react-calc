12001e046ad96ea152187b2c11e6209c8160eeb8
Chamon562 <Chamon562@gmail.com>
2020-08-26 23:43:47 -0700

    [add] Add numbers together with 2 input

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,20 +1,53 @@
 import React, { Component } from "react";
 import "./Button.css";
 
+
 class Calculator extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      num1: "",
+      num2: "",
+    };
+  }
+  setNum = (e, num) => {
+    //   the num inside is the key and the value of the key is e.target.value
+    this.setState({ [num]: e.target.value});
+  }
+
+  addNum = ( ) =>{
+      let addnum1 = parseInt(this.state.num1)
+      let addnum2 = parseInt(this.state.num2)
+
+      console.log(this.state)
+      this.setState({
+          result: addnum1 + addnum2
+          
+        })
+  }
+
   render() {
     return (
-      <div>
-        <div className="container">
-          <h1>Add with React!</h1>
-
-          <div className="add">
-            <input type="text" />
-            <span>+</span>
-            <input type="text" />
-            <button>=</button>
-            <h3>Addition results go here!</h3>
-          </div>
+      <div className="container">
+        <h1>Add with React!</h1>
+        <div className="add">
+          <input
+            type="number"
+            name="num1"
+            placeholder="Enter your first number"
+            value={this.state.num1}
+            onChange={(e) => this.setNum(e, "num1")}
+          />
+          <span>+</span>
+          <input
+            type="number"
+            name="num2"
+            placeholder="Enter Second Number"
+            value={this.state.num2}
+            onChange={(e) => this.setNum(e, "num2")}
+          />
+          <button onClick={this.addNum} >=</button>
+          <h3>{this.state.result}</h3>
         </div>
       </div>
     );
